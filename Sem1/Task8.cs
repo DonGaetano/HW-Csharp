0984b06d3d51d1a4d19c2b2b198d50c9efec9db0
DonGaetano <118386907+DonGaetano@users.noreply.github.com>
2022-12-04 12:18:01 +0300

    Update Task8.cs

--- Sem1/Task8.cs
+++ Sem1/Task8.cs
@@ -1,1 +1,14 @@
-Console.WriteLine("Hello World");
+// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.   
+  
+Console.WriteLine("Введите число N");  
+  
+string num1 = Console.ReadLine()!;  
+int parsed1 = int.Parse(num1);  
+  
+for (int i = 1; i <= parsed1; i++)  
+{  
+    if (i % 2 == 0)  
+    {  
+        Console.WriteLine(i); 
+    }  
+}
