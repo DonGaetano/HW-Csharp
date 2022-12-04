d892da4d488bf5615e4e5b64fa39230426894ab5
DonGaetano <118386907+DonGaetano@users.noreply.github.com>
2022-12-04 12:13:49 +0300

    Update Task4.cs

--- Sem1/Task4.cs
+++ Sem1/Task4.cs
@@ -1,1 +1,18 @@
-Console.WriteLine("Hello World");
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел. 
+ 
+Console.WriteLine("Введите 3 числа"); 
+ 
+string num1 = Console.ReadLine()!; 
+string num2 = Console.ReadLine()!; 
+string num3 = Console.ReadLine()!; 
+ 
+int parsed1 = int.Parse(num1); 
+int parsed2 = int.Parse(num2); 
+int parsed3 = int.Parse(num3); 
+ 
+int max = parsed1; 
+ 
+if (parsed2 > max) max = parsed2; 
+if (parsed3 > max) max = parsed3; 
+ 
+Console.WriteLine(max + " наибольшее");
