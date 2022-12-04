f214b74f4762f2b19bd5878b984b3b783ebd54f1
DonGaetano <118386907+DonGaetano@users.noreply.github.com>
2022-12-04 12:15:43 +0300

    Update Task6.cs

--- Sem1/Task6.cs
+++ Sem1/Task6.cs
@@ -1,1 +1,16 @@
-Console.WriteLine("Hello World");
+// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка). 
+ 
+Console.WriteLine("Введите число"); 
+ 
+string num1 = Console.ReadLine()!; 
+ 
+int parsed1 = int.Parse(num1); 
+ 
+if (parsed1 % 2 == 0) 
+{ 
+    Console.WriteLine("Число четное"); 
+} 
+else 
+{ 
+    Console.WriteLine("Число нечетное"); 
+}
