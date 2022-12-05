801cf5558299788596f89184bb3d32b12d1b88ba
DonGaetano <118386907+DonGaetano@users.noreply.github.com>
2022-12-05 16:45:16 +0300

    Update Task10.cs

--- Sem2/Task10.cs
+++ Sem2/Task10.cs
@@ -1,1 +1,18 @@
-Console.WriteLine("Hello World");
+// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трехзначное число");
+
+int number1 = int.Parse(Console.ReadLine()!);
+
+if (number1 >= 100 && number1 <= 999)
+{
+    int number2 = number1 / 10;
+    int number3 = number2 % 10;
+    Console.WriteLine($"Вторая цифра - {number3}");
+
+}
+else
+{
+    Console.WriteLine("Это число не трехзначное, идиот!");
+
+}
