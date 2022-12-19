efcbcca1b7d05d1920b1311e759ed471e8d5d184
DonGaetano <118386907+DonGaetano@users.noreply.github.com>
2022-12-19 18:26:35 +0300

    Create Task43.cs

---
+++ Sem6/Task43.cs
@@ -0,0 +1,31 @@
+//Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;  
+//значения b1, k1, b2 и k2 задаются пользователем. 
+
+Console.WriteLine("Введите значение b1");
+double b1 = double.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите значение k1");
+double k1 = double.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите значение b2");
+double b2 = double.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите значение k2");
+double k2 = double.Parse(Console.ReadLine()!);
+
+double xFromFunction = FindX(b1, k1, b2, k2);
+double yFromFunction = FindY(xFromFunction, k1, b1);
+
+Console.WriteLine($"Координаты точки пересечения двух прямых: ({xFromFunction};{yFromFunction})");
+
+double FindX(double b1f, double k1f, double b2f, double k2f)
+{
+    double calculatedX = Math.Round((b2f - b1f) / (k1f - k2f), 1);
+    return calculatedX;
+}
+
+double FindY(double calculatedX, double k1f, double b1f)
+{
+    double calculatedY = Math.Round(calculatedX * k1f + b1f, 1);
+    return calculatedY;
+}
