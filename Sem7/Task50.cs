d0fbdd1e6edce189706b9d032ccd954bb923e79d
DonGaetano <118386907+DonGaetano@users.noreply.github.com>
2022-12-22 11:42:59 +0300

    Create Task50.cs

---
+++ Sem7/Task50.cs
@@ -0,0 +1,59 @@
+//Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+//и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Console.WriteLine("Введите m");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите n");
+int n = int.Parse(Console.ReadLine()!);
+
+int[,] matrix = Create2DArray(m, n, -10, 10);
+Print2DArray(matrix);
+
+Console.WriteLine();
+
+Console.WriteLine("Введите первую позицию элемента");
+int positionM = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите вторую позицию элемента");
+int positionN = int.Parse(Console.ReadLine()!);
+
+FindElement(matrix, positionM, positionN);
+
+
+int[,] Create2DArray(int m, int n, int min, int max)
+{
+    int[,] matrix = new int[m, n];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void Print2DArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindElement(int[,] matrix, int position1, int position2)
+{
+    if (0 <= position1 && position1 < matrix.GetLength(0))
+    {
+        if (0 <= position2 && position2 < matrix.GetLength(1))
+        {
+            Console.WriteLine(matrix[position1, position2]);
+        }
+        else Console.WriteLine("Такого элемента не существует");
+    }
+    else Console.WriteLine("Такого элемента не существует");
+}
